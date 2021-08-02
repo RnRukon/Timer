b86e818dbd02f1b433cb5760675cb6d84cdcd8b0
Your Name <you@example.com>
2021-08-02 17:37:05 +0600

    one file deleted

--- index.js
+++
@@ -1,8 +0,0 @@
-
-for (var secondCount = 0; secondCount < 60; secondCount++) {
-
-    setInterval(console.log(secondCount), 1000);
-}
-// for (var minutesCount = 0; console.log(secondCount) > 60; minutesCount++) {
-//     console.log(minutesCount);
-// }

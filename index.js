ce92c6d8b6bde7e5bf5cb10ac97a809bd6a15c73
Your Name <you@example.com>
2021-08-02 17:30:34 +0600

    timer

---
+++ index.js
@@ -0,0 +1,8 @@
+
+for (var secondCount = 0; secondCount < 60; secondCount++) {
+
+    setInterval(console.log(secondCount), 1000);
+}
+// for (var minutesCount = 0; console.log(secondCount) > 60; minutesCount++) {
+//     console.log(minutesCount);
+// }
